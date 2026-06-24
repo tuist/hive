5f7ef4a5ba1b259b1dbd6ee1cff8214a9024290c
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-06-24 10:36:28 +0200

    fix(health): gate readiness on database availability

--- lib/hive_web/controllers/health_controller.ex
+++ lib/hive_web/controllers/health_controller.ex
@@ -1,7 +1,19 @@
 defmodule HiveWeb.HealthController do
   use HiveWeb, :controller
 
-  def ready(conn, _params) do
+  alias Hive.Repo
+
+  def live(conn, _params) do
     send_resp(conn, 200, "ok")
+  end
+
+  def ready(conn, _params) do
+    case Repo.query("SELECT 1", [], log: false) do
+      {:ok, _result} -> send_resp(conn, 200, "ok")
+      {:error, _reason} -> send_resp(conn, 503, "unavailable")
+    end
+  rescue
+    DBConnection.ConnectionError -> send_resp(conn, 503, "unavailable")
+    Postgrex.Error -> send_resp(conn, 503, "unavailable")
   end
 end
