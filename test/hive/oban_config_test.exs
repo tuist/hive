f67c7d5a628d7390c645a8e8b31ee55562d535c4
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-06-30 11:36:34 +0200

    fix(oban): rescue orphaned executing jobs

---
+++ test/hive/oban_config_test.exs
@@ -0,0 +1,15 @@
+defmodule Hive.ObanConfigTest do
+  use ExUnit.Case, async: true
+
+  test "rescues orphaned executing jobs" do
+    plugins =
+      :hive
+      |> Application.fetch_env!(Oban)
+      |> Keyword.fetch!(:plugins)
+
+    assert {Oban.Plugins.Lifeline, opts} =
+             Enum.find(plugins, &match?({Oban.Plugins.Lifeline, _opts}, &1))
+
+    assert Keyword.fetch!(opts, :rescue_after) == :timer.minutes(30)
+  end
+end
