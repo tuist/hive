59a229e209fb1d998ffb8c4e555790d5d6cdc5ef
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-07-07 11:55:22 +0200

    build(dependencies): update vulnerable packages

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Hive.MixProject do
       {:ecto_sql, "~> 3.13"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_view, "~> 1.0"},
+      {:phoenix_live_view, "~> 1.1.30"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
@@ -68,6 +68,7 @@ defmodule Hive.MixProject do
       {:bandit, "~> 1.5"},
       {:saxy, "~> 1.5"},
       {:sentry, "~> 13.0"},
+      {:hackney, "~> 4.5", override: true},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:mimic, "~> 1.7", only: :test}
     ]
